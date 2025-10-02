cdcdae9813e1cde0c4face21e02c17ab0ee9ad79
Ahmet Hakan YILDIRIM <hakan-336-ahmet@outlook.com>
2025-10-03 00:56:18 +0300

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -38,7 +38,7 @@ export const AboutSection = () => {
               <a
                 href="/CV.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
-                download
+                download="Ahmet-Hakan-Yildirim-Resume.pdf"
               >
                 Download CV
               </a>
