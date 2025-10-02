10877b0d900ce74e537cf4dd8b287182c3f4d54d
Ahmet Hakan YILDIRIM <hakan-336-ahmet@outlook.com>
2025-10-03 00:53:17 +0300

    bug fixed

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -36,8 +36,9 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/src/assets/CV.pdf"
+                href="/CV.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+                download
               >
                 Download CV
               </a>
